160b0e804c2de502e879dcbda4527a0fc39348f4
Buddika Kasun <buddikakasun80@gmail.com>
2026-06-09 13:12:27 +0530

    DB

--- backend/docker/postgres-auth/init.sql
+++ backend/docker/postgres-auth/init.sql
@@ -3,12 +3,19 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 SET timezone = 'UTC';
 
+-- User should already be created by POSTGRES_USER env variable
+-- But if you need to ensure it exists:
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'godayana') THEN
-        CREATE USER godayana WITH PASSWORD COALESCE(current_setting('POSTGRES_PASSWORD', true));
+        -- Password will be replaced by Railway's environment variable
+        CREATE USER godayana WITH PASSWORD '${POSTGRES_PASSWORD}';
     END IF;
 END
 $$;
 
+-- Grant privileges (use IF EXISTS to avoid errors)
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO godayana;
+
+-- Set schema search path
+ALTER DATABASE auth_db SET search_path TO public, auth;
